82e2ab005eae86d8914197f54da912a83c96ca59
daviu <daviu@DESKTOP-0BEA9Q2>
2023-11-23 19:34:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace PlaceHolders
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            //nome desse metodo e chamado de placeholders.
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            Console.WriteLine($"{nome} tem {idade} e tem saldo igual a {saldo:F2} reais ");
+
+            //Concatenação
+
+            Console.WriteLine(nome + " tem " + idade + " e tem saldo igual a " + saldo + " reais ");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
